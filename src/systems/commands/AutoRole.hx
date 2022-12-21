e74a3ee5c88061b6414f985443c3623968650927
j <Jarrio@users.noreply.github.com>
2022-12-21 16:58:56 +0000

    Add news role to users on join

--- src/systems/commands/AutoRole.hx
+++ src/systems/commands/AutoRole.hx
@@ -7,12 +7,14 @@ import Main;
 
 class AutoRole extends CommandBase {
 	@:fastFamily var users:{command:CommandForward, member:GuildMember};
+	final news_role_id:String = "1053111948255953006";
 	final event_role_id:String = "1054432874473996408";
 
 	override function update(_:Float) {
 		super.update(_);
 		iterate(users, (entity) -> {
 			member.roles.add(event_role_id).then(null, err);
+			member.roles.add(news_role_id).then(null, err);
 			this.universe.deleteEntity(entity);
 		});
 	}
