3ec95dc60e1c1e2e86b4d344317d1e3c898e5c5c
j <Jarrio@users.noreply.github.com>
2023-05-23 01:32:45 +0100

    clean up a file

--- src/commands/React.hx
+++ src/commands/React.hx
@@ -1,10 +1,7 @@
 package commands;
 
-import discord_js.TextChannel;
 import discord_builder.BaseCommandInteraction;
 import components.Command;
-import discord_js.Message;
-import Main.CommandForward;
 import systems.CommandBase;
 
 class React extends CommandBase {
@@ -13,9 +10,13 @@ class React extends CommandBase {
 			case React(message_id, emoji):
 				interaction.channel.messages.fetch(message_id).then(function(react_message) {
 					react_message.asType0.react(emoji).then(function(_) {
-						interaction.reply({content: '*reacted*', ephemeral: true}).then(null, (err) -> trace(err));
+						interaction.reply({content: '*reacted*', ephemeral: true})
+							.then(null, (err) -> trace(err));
 					}, (err) -> {
-						interaction.reply({ephemeral: true, content: '*failed to react, not sure why. invalid emoji perhaps? ask notbilly if no obvious reason*'});
+						interaction.reply(
+							{ephemeral: true,
+								content: '*failed to react, not sure why. invalid emoji perhaps? ask notbilly if no obvious reason*'}
+						);
 						trace(err);
 					});
 				});
