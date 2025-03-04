48b3b703142bef9b79b1ce4da768a3d860431be9
j <748557+Jarrio@users.noreply.github.com>
2025-03-04 18:44:06 +0000

    added a new field to the table

--- src/database/types/DBState.hx
+++ src/database/types/DBState.hx
@@ -6,6 +6,7 @@ class DBState extends MyRecord {
 	@record public var id:Int;
 	@record public var key:String;
 	@record public var value:Dynamic;
+	@record public var int:Null<Int>;
 
 	public function new() {}
 }
